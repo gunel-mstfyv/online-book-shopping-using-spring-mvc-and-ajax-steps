1a6a8343ac60cca0e75896e02586af2f90a6d74d
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-11-19 22:40:51 +0400

    spring project created

---
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/BookshoppingGunelMApplication.java
@@ -0,0 +1,13 @@
+package az.developia.bookshoppinggunelm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshoppingGunelMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshoppingGunelMApplication.class, args);
+	}
+
+}
---
+++ projects/bookshopping-gunel-m/src/test/java/az/developia/bookshoppinggunelm/BookshoppingGunelMApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.bookshoppinggunelm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshoppingGunelMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
