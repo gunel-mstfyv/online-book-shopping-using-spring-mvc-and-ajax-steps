68cfa043d17b354563b32edb32535f833d6c986a
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-11-21 14:37:45 +0400

    new book registration

--- projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/BookController.java
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/BookController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import az.developia.bookshoppinggunelm.dao.BookDAO;
 import az.developia.bookshoppinggunelm.model.Book;
 
@@ -21,5 +24,19 @@ public class BookController {
 		List<Book> books = bookDAO.findAll();
 		model.addAttribute("books", books);
 		return "books";
+	}
+	@GetMapping(path="/books/new")
+	public String openNewBookPage(Model model) {
+		Book book=new Book();
+		model.addAttribute("book", book);
+			return "new-book";
+	}
+	
+	@PostMapping(path="/books/new-book-process")
+	public String saveBook(@ModelAttribute(name="book") Book book,Model model) {
+		bookDAO.save(book);
+		List<Book> books = bookDAO.findAll();
+		model.addAttribute("books", books);
+	    return "books";
 	}
 }
