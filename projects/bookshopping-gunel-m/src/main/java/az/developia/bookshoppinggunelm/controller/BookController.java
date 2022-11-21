41d912dd1cba9828d72253365da6d3db84488174
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-11-22 01:48:31 +0400

    edit delete redirect

--- projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/BookController.java
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/BookController.java
@@ -1,12 +1,14 @@
 package az.developia.bookshoppinggunelm.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import az.developia.bookshoppinggunelm.dao.BookDAO;
@@ -29,6 +31,7 @@ public class BookController {
 	public String openNewBookPage(Model model) {
 		Book book=new Book();
 		model.addAttribute("book", book);
+		model.addAttribute("header", "New Book");
 			return "new-book";
 	}
 	
@@ -41,4 +44,30 @@ public class BookController {
 		model.addAttribute("books", books);
 	    return "books";
 	}
+	
+	@GetMapping(path="/books/delete/{id}")
+	public String deleteBook(@PathVariable(name="id") Integer id,Model model) {
+		boolean bookExists=bookDAO.findById(id).isPresent();
+		if(bookExists) {
+		bookDAO.deleteById(id);}
+		else {}
+		List<Book> books = bookDAO.findAll();
+		model.addAttribute("books", books);
+			return "redirect:/books";
+	}
+	
+	@GetMapping(path="/books/edit/{id}")
+	public String editBook(@PathVariable(name="id") Integer id,Model model) {
+		Optional<Book> bookOptional=bookDAO.findById(id);
+		boolean bookExists=bookOptional.isPresent();
+		Book book =new Book();
+		if(bookExists) {
+		book=bookOptional.get();
+		}
+		else {}		
+		model.addAttribute("book", book);
+		model.addAttribute("header", "Edit The Book");
+			return "new-book";
+	}
+	
 }
