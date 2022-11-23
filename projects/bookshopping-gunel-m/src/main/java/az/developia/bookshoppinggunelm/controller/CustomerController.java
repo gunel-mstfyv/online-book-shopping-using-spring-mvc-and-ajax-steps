b10ff455305e7db2511aa999bc08c29283100332
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-11-24 00:12:26 +0400

    047 step tymeleaf external css

--- projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/CustomerController.java
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/CustomerController.java
@@ -1,12 +1,20 @@
 package az.developia.bookshoppinggunelm.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerController {	
 	@GetMapping(path="/customer")
-	public String showCustomerPage() {		
+	public String showCustomerPage(Model model) {
+		ArrayList<String> books =new ArrayList<>();
+		for (int i = 0; i < 100; i++) {
+			books.add("");
+		}
+		model.addAttribute("books", books);
 		return "customer";
 	}
 	
