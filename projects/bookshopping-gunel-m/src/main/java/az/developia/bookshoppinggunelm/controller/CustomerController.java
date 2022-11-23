bff835146d8fb69d8f07eb37c9d989a8738e38ed
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-11-24 01:10:28 +0400

    052 steps basket created badge used

--- projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/CustomerController.java
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/CustomerController.java
@@ -1,19 +1,25 @@
 package az.developia.bookshoppinggunelm.controller;
 
-import java.util.ArrayList;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import az.developia.bookshoppinggunelm.dao.BookDAO;
+import az.developia.bookshoppinggunelm.model.Book;
+
 @Controller
 public class CustomerController {	
+	
+	@Autowired
+	private BookDAO bookDAO;
+	
 	@GetMapping(path="/customer")
 	public String showCustomerPage(Model model) {
-		ArrayList<String> books =new ArrayList<>();
-		for (int i = 0; i < 100; i++) {
-			books.add("");
-		}
+		List<Book> books = bookDAO.findAll();
 		model.addAttribute("books", books);
 		return "customer";
 	}
