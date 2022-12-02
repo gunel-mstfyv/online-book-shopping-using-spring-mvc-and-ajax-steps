70ec6b9fc055e2755e49f339045fd50d221d43d5
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-12-02 18:46:03 +0400

    step 61 has done

--- projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/UserController.java
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/controller/UserController.java
@@ -2,6 +2,7 @@ package az.developia.bookshoppinggunelm.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import az.developia.bookshoppinggunelm.dao.UserDAO;
 import az.developia.bookshoppinggunelm.model.User;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserDAO userDAO;
 	
 	private boolean userCreated=false;
 
@@ -38,7 +43,11 @@ public class UserController {
 		if(result.hasErrors()) {
 			return "create-account";
 		}
-		System.out.println(user);
+		boolean userExists=userDAO.createUser(user);
+		if(userExists) {
+			model.addAttribute("userExists", "");
+			return "create-account";
+		}
 		userCreated=true;
 	    return "redirect:/show-login";
 	}
