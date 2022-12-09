fa83d3963865e4aa16cdccf4bb31b2420e291b48
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-12-10 01:03:09 +0400

    init metodu has created

--- projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/file/FileSystemStorageService.java
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/file/FileSystemStorageService.java
@@ -1,7 +1,11 @@
 package az.developia.bookshoppinggunelm.file;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,10 +13,20 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class FileSystemStorageService implements StorageService {
 
+	
+	private final Path rootLocation;
+	@Autowired
+	public FileSystemStorageService(StorageProperties properties) {
+		this.rootLocation=Paths.get(properties.getLocation());
+	}
+	
 	@Override
 	public void init() {
-		
-		
+		try {
+			Files.createDirectories(rootLocation);
+		} catch (IOException e) {
+			throw new StorageException("Folder could not be created!",e);
+		}		
 	}
 
 	@Override
