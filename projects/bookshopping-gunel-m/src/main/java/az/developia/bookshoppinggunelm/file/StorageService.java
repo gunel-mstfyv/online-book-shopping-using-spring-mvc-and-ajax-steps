b765b34282eeaea29d0758ee3022a066552e2ccc
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-12-10 00:41:01 +0400

    StorageService file metodlarin teyini

--- projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/file/StorageService.java
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/file/StorageService.java
@@ -1,5 +1,15 @@
 package az.developia.bookshoppinggunelm.file;
 
+import java.nio.file.Path;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface StorageService {
 
+	void init();
+	String store(MultipartFile file);
+	Path load(String filename);
+	Resource loadAsResource(String filename);
+	void deleteAll();
 }
