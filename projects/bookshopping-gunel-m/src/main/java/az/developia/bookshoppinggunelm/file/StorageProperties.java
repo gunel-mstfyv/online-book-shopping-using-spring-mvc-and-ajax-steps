868b73b0a968bbce32c6cf8ce2763d214fafdcc3
KMGunel <89314536+KMGunel@users.noreply.github.com>
2022-12-10 00:34:17 +0400

    Storage Properties sinfi hazirlandi

--- projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/file/StorageProperties.java
+++ projects/bookshopping-gunel-m/src/main/java/az/developia/bookshoppinggunelm/file/StorageProperties.java
@@ -1,5 +1,11 @@
 package az.developia.bookshoppinggunelm.file;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+@Data
+@ConfigurationProperties("storage")
 public class StorageProperties {
 
+	private String location = "upload-dir-book-shopping-ajax";
 }
